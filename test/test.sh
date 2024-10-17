a8fa1704bc9deb110623b842b27d2ea6cc410c44
kishtarn555 <Hector>
2024-10-16 20:17:18 -0600

    [Refactor][Test] Separate scripts into files

--- test/test.sh
+++ test/test.sh
@@ -3,54 +3,7 @@ set -e
 
 ROOT="$(git rev-parse --show-toplevel)"
 
-echo "Test Java"
 
-for problem in "${ROOT}/test/test-suite/"*; do
-	problem_name=$(basename "${problem}")
+"${ROOT}/test/test.java.sh"
 
-	npx rekarel compile "${problem}/solution.kp" -o "${ROOT}/bin/${problem_name}.kx"
-
-    echo -n "${problem_name} .................. RUNING                      "
-    start_time=$(date +%s)
-
-	for casename in "${problem}/cases"/*.in; do
-        case_name=$(basename "${casename}")
-
-		{
-            npx rekarel run "${ROOT}/bin/${problem_name}.kx" < "${casename}" | diff -Naurw --ignore-blank-lines "${casename%.in}.out" -
-        } &
-	done
-    wait
-    # End the timer
-    end_time=$(date +%s)
-    
-    # Calculate the elapsed time
-    elapsed_time=$((end_time - start_time))
-    echo -ne "\r${problem_name} .................. DONE ${elapsed_time} seconds                               \n"
-done
-
-
-echo "Test Java"
-
-for problem in "${ROOT}/test/test-suite/"*; do
-	problem_name=$(basename "${problem}")
-
-	npx rekarel compile "${problem}/solution.kp" -o "${ROOT}/bin/${problem_name}.kx"
-
-    echo -n "${problem_name} .................. RUNING                      "
-    start_time=$(date +%s)
-	for casename in "${problem}/cases"/*.in; do
-        case_name=$(basename "${casename}")
-
-		{
-            npx rekarel run "${ROOT}/bin/${problem_name}.kx" < "${casename}" | diff -Naurw --ignore-blank-lines "${casename%.in}.out" -
-        } &
-    done
-    wait
-    # End the timer
-    end_time=$(date +%s)
-    
-    # Calculate the elapsed time
-    elapsed_time=$((end_time - start_time))
-    echo -ne "\r${problem_name} .................. DONE ${elapsed_time} seconds                               \n"
-done
+"${ROOT}/test/test.pascal.sh"
